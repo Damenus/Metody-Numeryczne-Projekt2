827030cc6d1fb977cfb6d40f835b71b2268aac36
Damenus <darczuk.damian@gmail.com>
2016-04-04 13:17:41 +0200

    Zrobiony MonteCarlo i Gauss

--- Numeryczne_Projekt2/Numeryczne_Projekt2.cpp
+++ Numeryczne_Projekt2/Numeryczne_Projekt2.cpp
@@ -4,6 +4,8 @@
 #include <map>
 #include <queue>
 #include <set>
+#include <ctime>
+#include <cstdlib>
 
 
 using namespace std;
@@ -32,10 +34,12 @@ class ruch
 {	
 //(pole gdzie gracz1, pole gdzie gracz2, który gracz ma ruch) 
 public: 
-	int gracz1Pozycja, gracz2Pozycja, gracz, wynik;
+	int gracz1Pozycja, gracz2Pozycja, gracz;
 	vector<ruchw> tab;
 	int pewne = 0;
 	int numer = 0;
+	double wynik = 1.0;
+	double wynik2 = 0.0;
 
 public:
 	ruch() {
@@ -78,10 +82,10 @@ public:
 		rozmiar_macierzy = rozmiar;
 		macierz = new double*[rozmiar];
 		for (int i = 0; i < rozmiar; i++)
-			macierz[i] = new double[rozmiar + 1]; //+1 na macierz wyrazów wolnych
+			macierz[i] = new double[rozmiar]; //+1 na macierz wyrazów wolnych
 
 		for (int j = 0; j < rozmiar; j++) {
-			for (int k = 0; k <= rozmiar; k++){
+			for (int k = 0; k < rozmiar; k++){
 				macierz[j][k] = 0;
 			}
 		}
@@ -114,37 +118,177 @@ public:
 			}
 			
 			this->macierz[i][i] = 1;
-			this->macierz[i][wektor.size() + 1] = (double)wektor[i].pewne / 6.0;
+			this->macierz[i][wektor.size()] = (double)wektor[i].pewne / 6.0;
 		}
 		
+	}
+
+	void wypelnienieGauss(vector<ruch> wektor) {
+		double a, b;
+		int c, d;
+		for (int i = 0; i < wektor.size(); i++) {
+			for (int j = 0; j < wektor[i].tab.size(); j++) {
+				//c = wektor[i].tab[j].niewiadoma->numer;
+				c = znajdz_numer_kolumny(wektor, wektor[i].tab[j].niewiadoma);
+				d = -wektor[i].tab[j].wspolczynnik;
+				this->macierz[i][c] = (double)d / 6.0;
+
+			}
+
+			this->macierz[i][i] = 1;
+			this->macierz[i][wektor.size()] = (double)wektor[i].pewne / 6.0;
+		}
+
 	}
 
 	void wypisz_macierz() {
 		for (int j = 0; j < rozmiar_macierzy; j++) {
 			cout << j << ": ";
-			for (int k = 0; k < rozmiar_macierzy+2; k++){
+			for (int k = 0; k <= rozmiar_macierzy; k++){
 				cout << macierz[j][k] << " ";
 			}
 			cout << endl;
 		}
 	}
 
-	void gauss() {
+	void gauss(vector<ruch> wektor) {
 
 		//eliminacja
 		double m;
 		for (int i = 0; i < this->rozmiar_macierzy; i++) { // ka¿da kolumna pokolei
 
-			for (int j = 1; j < this->rozmiar_macierzy; j++) {
-				if (macierz[0][i] != 0) { // zero
-					m = macierz[j][i] / macierz[0][i]; //wspo³czynnik mno¿enia pierwszego weirsza, by odj¹æ od kolejnych kolumn, by wyzerowaæ dolny trójk¹t
-					for (int k = 0; k < this->rozmiar_macierzy + 1; k++) { // zerowanie kolumny
-						macierz[j][k] = macierz[j][k] - m * macierz[0][k];
-					}
-				}
+			for (int j = i+1; j < this->rozmiar_macierzy; j++) {
+
+				m = macierz[j][i] / macierz[i][i]; //wspo³czynnik mno¿enia pierwszego weirsza, by odj¹æ od kolejnych kolumn, by wyzerowaæ dolny trójk¹t
+				for (int k = i; k < this->rozmiar_macierzy + 1; k++) { // zerowanie kolumny
+					macierz[j][k] = macierz[j][k] - m * macierz[i][k];
+				}				
 			}
 		}
+
+		//wypisz_macierz();
+
+		double s = 0;
+		//obliczanie
+		int n = wektor.size() - 1;
+		wektor[n].wynik = macierz[n][n + 1] / macierz[n][n];
+
+		for (int i = n - 1; i >= 0; i--){
+			s = 0;
+			for (int k = i + 1; k <= n; k++) {
+				s = s + macierz[i][k] * wektor[k].wynik;
+				wektor[i].wynik = (macierz[i][n + 1] - s) / macierz[i][i];
+			}
+		}
+
+		cout << endl << wektor[0].wynik << endl;
+
 	}
+
+	void iteracyjne(vector<ruch> wektor) {
+
+		double c;
+		for (int obieg = 0; obieg < 5; obieg++) {
+
+			for (int i = 0; i < rozmiar_macierzy; i++) {//dla ka¿dego wiersza
+				for (int j = 0; j < rozmiar_macierzy; j++) {
+					if (i != j) {
+						c = this->macierz[i][j] * wektor[j].wynik2;
+						wektor[i].wynik2 -= c;
+					}
+				}
+				wektor[i].wynik2 += this->macierz[i][rozmiar_macierzy]; // wyrazy wolne
+			}
+
+			for (int i = 0; i < wektor.size(); i ++)
+				cout << i << ": " << wektor[i].wynik2 << endl;
+		}
+
+		cout  << wektor[0].wynik2 << endl;
+	}
+};
+
+class Plansza{
+public:
+	int ilosc_pol;
+	map<int, int> polapki;
+
+	Plansza(int ilosc_pol,	map<int, int> polapki) {
+		this->ilosc_pol = ilosc_pol;
+		this->polapki = polapki;
+	}
+};
+
+class MonteCarlo {
+public:
+	Plansza *plansza;
+	int ilosc_gier = 1000000;
+
+	MonteCarlo(Plansza *plansza) {
+		this->plansza = plansza;
+		srand(time(NULL));
+	}
+
+	int rzutKoscia() {
+		return ( (rand() % 6) + 1 );
+	}
+
+	double wynik() {
+		
+		int tablica_wygranych[2] = { 0, 0 };
+
+		for (int przebieg = 0; przebieg < this->ilosc_gier; przebieg++) {
+			tablica_wygranych[gra()]++;
+		}
+
+		return (double)tablica_wygranych[0] / (double)this->ilosc_gier;
+
+	}
+
+	int gra() {
+		//pozycja na planszy graczy
+		int gracz1 = 0;
+		int gracz2 = 0;
+		int czyja_tura = 1;
+		int rzut_koscia;
+
+		while (true) {
+
+			rzut_koscia = rzutKoscia();
+
+			if (czyja_tura == 1) {
+				gracz1 += rzut_koscia;
+
+				if (plansza->polapki.find(gracz1) != plansza->polapki.end()) { // trafi³ na pu³pakê
+					gracz1 = gracz1 + plansza->polapki[gracz1];
+				}
+				
+				if (gracz1 >= plansza->ilosc_pol) { //sprawdzenie czy wygra³
+					return 0;
+					break;
+				}
+
+			}
+			else {
+				gracz2 += rzut_koscia;
+
+				if (plansza->polapki.find(gracz2) != plansza->polapki.end()) { 
+					gracz2 = gracz2 + plansza->polapki[gracz2];
+				}
+
+				if (gracz2 >= plansza->ilosc_pol) {
+					return 1;
+					break;
+				}
+
+			}
+
+			czyja_tura = czyja_tura % 2 + 1; //modulo 2 + 1 daje przeciwnego gracza
+
+		}		
+
+	}
+
 
 };
 
@@ -267,10 +411,23 @@ int main()
 
 
 	Macierz *m = new Macierz(wektor2.size());
+//	m->wypelnienieGauss(wektor2);
+//	m->wypisz_macierz();
+//	m->gauss(wektor2);
+//	m->wypisz_macierz();
+
 	m->wypelnienie(wektor2);
 	m->wypisz_macierz();
-	m->gauss();
+	m->iteracyjne(wektor2);
 	m->wypisz_macierz();
+
+
+	//Plansza *plansza = new Plansza(pola, polapki);
+	//MonteCarlo *gra = new MonteCarlo(plansza);
+	//cout << gra->wynik() << endl;;
+
+
+
 	return 0;
 }
 
